3c977b34bd08cb85899b7fbb7116d1d91681b818
John C Harrison <john@johnsmacbook.local>
2023-08-17 20:15:04 -0400

    Init project with base app

---
+++ cst339/src/main/java/com/gcu/model/SampleModel.java
@@ -0,0 +1,31 @@
+package com.gcu.model;
+
+public class SampleModel {
+    private long id;
+    private String name;
+
+    public SampleModel() {
+    }
+
+    public SampleModel(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
