3db1d327d2a7465f6dc4eb6f02ca657a0b94a9cb
John C Harrison <john@johnsmacbook.local>
2023-08-20 21:58:00 -0400

    okay that should be everything

--- cst339/src/main/java/com/gcu/controller/RegistrationController.java
+++ cst339/src/main/java/com/gcu/controller/RegistrationController.java
@@ -1,34 +1,31 @@
 package com.gcu.controller;
 
 import com.gcu.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
 @RequestMapping("/registration")
 public class RegistrationController {
 
+    @GetMapping("")
+    public String registration(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
 
-	@GetMapping("")
-	public String showRegistrationForm(Model model) {
-		model.addAttribute("user", new User());
-		return "registration";
-	}
-
-	@PostMapping("/register")
-	public String registerUser(@ModelAttribute("user") @Validated User user, BindingResult bindingResult) {
-		if (bindingResult.hasErrors()) {
-			return "registration";
-		}
-		return "redirect:/login"; // Redirect to login page
-	}
+    @PostMapping("/register")
+    public String registerUser(@Validated User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+        System.out.println("...this is where we would save the user in the database...IF WE HAD ONE");
+        return "redirect:/login"; // Redirect to login page
+    }
 
 }
--- cst339/src/main/java/com/gcu/model/User.java
+++ cst339/src/main/java/com/gcu/model/User.java
@@ -12,21 +12,22 @@ public class User {
 
 	// @Id
 	// @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	// private Long id;
 	private String firstName;
 	private String lastName;
 	private String email;
-	private String phoneNumber;
-	private String username;
 	private String password;
 
-	public Long getId() {
-		return this.id;
+	public User() {
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+	// public Long getId() {
+	// return this.id;
+	// }
+
+	// public void setId(Long id) {
+	// this.id = id;
+	// }
 
 	public String getFirstName() {
 		return firstName;
@@ -58,22 +59,6 @@ public class User {
 
 	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
 	}
 
 	// Constructors, getters, setters
