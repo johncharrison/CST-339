bbc5f31ac5a10e15e854e1e4ec3c48d388ed0c02
Calvin Clocuh <cclocuh@msn.com>
2023-08-26 18:47:55 -0700

    refactor registration module

--- cst339/src/main/java/com/gcu/model/User.java
+++ cst339/src/main/java/com/gcu/model/User.java
@@ -1,18 +1,7 @@
 package com.gcu.model;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.Table;
-
-// @Entity
-// @Table(name = "users")
 public class User {
 
-	// @Id
-	// @GeneratedValue(strategy = GenerationType.IDENTITY)
-	// private Long id;
 	private String firstName;
 	private String lastName;
 	private String email;
